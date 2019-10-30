0f5e3465cdfc95db21cbb95c48ae0b6384128554
Cristhian Laurente <frontend.crstn@gmail.com>
2019-10-30 06:52:20 -0600

    Snippets de html configurada para Bootstrap

---
+++ Practicas_Personales/js/01-arrays.js
@@ -0,0 +1,19 @@
+window.onload = () =>{
+
+    let fruit = ['apple','banana'];
+    // console.log(fruit.length);
+    // console.log(fruit[0]);
+    // let otro = fruit[fruit.length-1]
+    // console.log(otro);
+    
+
+    let  uno = fruit.length-1
+
+    
+    for (let i = 0; i < uno; i++) {
+        
+        console.log(fruit);
+        
+    }
+        
+}
