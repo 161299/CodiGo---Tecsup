bbea4569a424bd9f0c8e42bdff37ec2557722f08
Cristhian Laurente <frontend.crstn@gmail.com>
2019-11-04 07:00:49 -0600

    Materialize

--- Practicas_Personales/js/01-arrays.js
+++ Practicas_Personales/js/01-arrays.js
@@ -1,19 +1,41 @@
 window.onload = () =>{
 
     let fruit = ['apple','banana'];
-    // console.log(fruit.length);
-    // console.log(fruit[0]);
-    // let otro = fruit[fruit.length-1]
-    // console.log(otro);
+    // imprime la cantidad de espacios que hay en nuestro array
+    console.log(fruit.length);
+
+    // aqui le indico que estoy imprimiendo la posicion 1 que en este caso seria banana
+    console.log(fruit[1]);
+
+    // aqui imprimo el array commpleto
+    console.log(fruit)
+    
+    // imprimo  el contenido de la posicion 0 
+    let first = fruit[0];
+    console.log(first);
+
+    // Otra manera de imprimir el contenido de la posicion 0
+    let otro = fruit[fruit.length-2]
+    console.log(otro);
     
 
-    let  uno = fruit.length-1
-
-    
-    for (let i = 0; i < uno; i++) {
+    for (let i = 0; i < fruit.length; i++) {
         
-        console.log(fruit);
+        console.log(fruit[i]);
+        
         
     }
+    
+    // loop over an array
+    // Recorriendo un array
+    // fruit.forEach(function(item,index,array){
+    //     console.log(item,index);
+        
+    // })
+
+    
+    
+
+
         
 }
