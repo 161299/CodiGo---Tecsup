3ca4ab90e000c66cea237b14de29128c61c3e935
Cristhian Laurente <frontend.crstn@gmail.com>
2019-12-10 09:00:06 -0500

    tarea no heccha

--- Semana11/Dia1/PruebaExpress/dist/index.js
+++ Semana11/Dia1/PruebaExpress/dist/index.js
@@ -17,6 +17,29 @@ app.post("/persona", (req, res) => {
         content: objPersona
     });
 });
+app.get("/persona/:id_persona", (req, res) => {
+    let id = req.params.id_persona;
+    for (let i = 0; i < listaPersona.length; i++) {
+        if (JSON.stringify(listaPersona[i].id) == id) {
+            console.log(id);
+            console.log("-------------");
+            console.log(JSON.stringify(listaPersona[i].id));
+            console.log(JSON.parse(listaPersona[i].id));
+            let objPersona = listaPersona[i];
+            res.json({
+                success: true,
+                content: objPersona
+            });
+        }
+        else {
+            res.json({
+                success: false,
+                message: "El id ingresado no existe"
+            });
+        }
+        ;
+    }
+});
 app.get("/personas", (req, res) => {
     let objPersona = listaPersona;
     res.json({
@@ -25,14 +48,33 @@ app.get("/personas", (req, res) => {
     });
 });
 app.put('/persona/:id_persona', (req, res) => {
-    let objPersona = listaPersona['id_persona'].push(req.body);
-    res.json({
-        ok: true,
-        content: objPersona
-    });
+    let id = req.params.id_persona;
+    for (let i = 0; i < listaPersona.length; i++) {
+        if (JSON.stringify(listaPersona[i].id) == id) {
+            let objPersona = listaPersona.push(req.body);
+            let NewObjPersona = listaPersona;
+            res.json({
+                success: true,
+                message: "Se actualizo perfectamente",
+                content: objPersona,
+                new: NewObjPersona
+            });
+        }
+        else {
+            res.json({
+                success: false,
+                message: "El id ingresado no existe"
+            });
+        }
+    }
 });
 app.delete('persona/:id_persona', (req, res) => {
     let objPersona = req.body;
+    let id = req.params.id_persona;
+    for (let i = 0; i < listaPersona.length; i++) {
+        if (JSON.stringify(listaPersona[i].id) == id)
+            listaPersona[i].splice();
+    }
     res.json({
         ok: true,
         message: "Se elemino a persona"
--- Semana11/Dia1/PruebaExpress/index.ts
+++ Semana11/Dia1/PruebaExpress/index.ts
@@ -17,6 +17,27 @@ app.post("/persona", (req: Request, res: Response) => {
   });
 });
 
+app.get("/persona/:id_persona", (req: Request, res: Response) => {
+  let id = req.params.id_persona  
+  for (let i = 0; i < listaPersona.length; i++) {
+    if(JSON.stringify(listaPersona[i].id) == id){
+      console.log(id);
+      console.log("-------------"); 
+      console.log(JSON.stringify(listaPersona[i].id));   
+      console.log(JSON.parse(listaPersona[i].id));   
+        let objPersona = listaPersona[i]
+        res.json({
+            success: true, 
+            content: objPersona
+        })
+    }else{
+      res.json({
+        success: false,
+        message: "El id ingresado no existe"
+      })
+   };
+  }
+});
 app.get("/personas", (req: Request, res: Response) => {
     let objPersona = listaPersona
     res.json({
@@ -27,16 +48,36 @@ app.get("/personas", (req: Request, res: Response) => {
 
 
 app.put('/persona/:id_persona', (req: Request, res:Response)=>{
-    let objPersona = listaPersona['id_persona'].push(req.body)
-
-    res.json({
-        ok: true,
-        content: objPersona
-    })
+    let id = req.params.id_persona
+    for (let i = 0; i < listaPersona.length; i++) {
+      if(JSON.stringify(listaPersona[i].id) == id){
+          let objPersona = listaPersona.push(req.body)
+          let NewObjPersona = listaPersona
+          res.json({
+              success: true, 
+              message: "Se actualizo perfectamente",
+              content: objPersona,
+              new : NewObjPersona
+          })
+      }else{
+        res.json({
+          success: false,
+          message: "El id ingresado no existe"
+        })
+        
+      }
+      
+    }
+    
 })
 
 app.delete('persona/:id_persona', (req: Request, res: Response) => {
     let objPersona = req.body
+    let id = req.params.id_persona
+    for (let i = 0; i < listaPersona.length; i++) {
+      if(JSON.stringify(listaPersona[i].id) == id)
+      listaPersona[i].splice()
+    }
     res.json({ 
          ok: true,
          message: "Se elemino a persona"
